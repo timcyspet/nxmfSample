dfdc43d99c0ba6661a66cfbca9d2a50a1343dec6
John Thomas <John.Thomas+EYGS@ie.ey.com>
2023-08-08 09:34:15 +0100

    Sample Project setup
    Sample Project setup

---
+++ src/nxmf/apps/about/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nxmf-about-entry',
+  template: `<nxmf-nx-welcome></nxmf-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
